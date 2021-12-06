b649c59ecadb86fcc7d5aaa462cf4b015335433f
RoyZion <royzion94@gmail.com>
2021-12-06 16:21:41 +0200

    Products Page

---
+++ Products.js
@@ -0,0 +1,8 @@
+function ShowHideDiv(Products) {
+    var x = document.getElementById(this.Products);
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
