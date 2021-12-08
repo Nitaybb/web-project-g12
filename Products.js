e8453ea18a94bfdcf0923de83e286c76803872d5
Nitaybb <nitay.bb@gmail.com>
2021-12-08 17:53:37 +0200

    our new project

--- Products.js
+++
@@ -1,8 +0,0 @@
-function ShowHideDiv(Products) {
-    var x = document.getElementById(this.Products);
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-}
